f0ac22d337cbe44a455d66a23f3398a88e03f97d
Riley Watts <rileywatts.yes@gmail.com>
2017-02-24 10:11:21 -0800

    add remainder functionality to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,57 @@
+
+
+var addTo = function(input) {
+  var total = 0;
+  var holdMe = [];
+  for(var i = 0; i < input; i++) {
+    total += 1;
+    holdMe.push(total);
+  }
+  return holdMe;
+}
+
+var factorThree = function(input) {
+  var inputSlice = input.slice();
+  var holdMe = [];
+  for(var i=0; i < input.length; i++) {
+    if(inputSlice[i]%3===0) {
+      holdMe.push("ping");
+    }
+    else {
+      holdMe.push(inputSlice[i]);
+    }
+  console.log(holdMe);
+  }
+}
+
+var factorFive = function(input) {
+  var inputSlice = input.slice();
+  var holdMe = [];
+  for(var i=0; i < input.length; i++)
+    if(inputSlice[i]%5===0) {
+      holdMe.push("pong");
+    }
+    else {
+      holdMe.push(inputSlice[i]);
+    }
+  console.log(holdMe);
+}
+
+
+
+
+
 $(function() {
  $("#formOne").submit(function(event) {
    event.preventDefault();
-   input = $("#userInput").val();
-   console.log(input);
+   userInput = $("#userInput").val();
+
+   var inputArray = addTo(userInput);
+   factorThree(inputArray);
+   factorFive(inputArray);
+  //  console.log(modInput);
+  //  factorThree(userInput);
+  //  console.log();
 
  });
 });
